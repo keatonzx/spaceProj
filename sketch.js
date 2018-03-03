dcea16dd7fca66976076daf93010890dd1a0dc7b
Keaton Armstrong <keatondirect@icloud.com>
2018-03-02 22:33:50 -0800

    added some planets, changed scale

--- sketch.js
+++ sketch.js
@@ -15,26 +15,38 @@ function draw() {
 	ship.updateShip();
 	ship.showShip(10,10);
 
-	fill(0);
-	rect(300,height-50,50,50);
-	 
+	//fill(0);
+	//rect(100,height-50,1,50);
+	var count = 0;
+	// 2min :50 seconds
 	for (let i = 0; i < 2000; i++){
 		fill(0);
-		rect(300 * i,height-50,50,50);
-		console.log(300*i);
-		//174900 pixels probably need over a million, will do math later 
+		rect(100*i,height-50,1,50);
+		//count = count + 1;
+		count++;
+
 	}
-	
+	fill(255,0,0);
+	ellipse(-10,height/2,50,height);
+	ellipse(500,height/2,50,50);
+	ellipse(5000,height/2,50,50);
+	ellipse(50000,height/2,50,50);
+
+
 	checkKeyDown();
 }
 
 function checkKeyDown() {
 	if (keyIsDown(RIGHT_ARROW)) {
+		if(deltaSpeed < 20)
 		deltaSpeed = deltaSpeed +1 *2
 		//console.log(deltaSpeed);
 		ship.direction(1);
 	} 
 	if (keyIsDown(LEFT_ARROW)) {
+		
+		//while(deltaSpeed > -20 && ship.x > 1)
 		deltaSpeed = deltaSpeed +1 *-2
+		
 	} 
 }
