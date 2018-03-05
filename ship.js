934c200174f6c16f873f3d558872370717bc740a
Keaton Armstrong <keatondirect@icloud.com>
2018-03-05 14:09:06 -0800

    trying to optimize the project, much harder then originally anticipated

--- ship.js
+++ ship.js
@@ -1,24 +1,25 @@
 function Ship(){
 	this.x = 1;
 	this.xspeed = 1;
+	this.dist = 0;
+
+	this.updateDist= function(){
+		return this.x;
+	}
 
 	this.updateShip = function(){
 		this.x = this.x + this.xspeed*deltaSpeed;
-		//this.y = this.y + this.yspeed*deltaSpeed;
-
 		this.x = constrain(this.x, -500,100000000);
-		//this.y = constrain(this.y, 0, height-deltaSpeed);
 
 	}
+
 	this.showShip = function(x,y){
 		fill(180,0,180);
-		text('hello',this.x-deltaSpeed,200);
-		
+		text(this.dist+this.x,this.x-deltaSpeed,200);
 		rect(this.x - deltaSpeed,height/2,x,y);
 		}
 
 	this.direction = function(x){
-		this.xspeed = x;
-		
+		this.xspeed = x;	
 	}	
 }
--- sketch.js
+++ sketch.js
@@ -5,58 +5,52 @@ var count = 0;
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	ship = new Ship();
+
 }
 
 function draw() {
+	//frameRate(10);
 	background(51);
 	
 	translate(-ship.x,0);
 	ship.updateShip();
 	ship.showShip(10,10);
 	drawScale();
-
-	// 2min :50 seconds
-	fill(150);
-	
-
-	fill(255,0,0);
-	//ellipse(-10,height/2,100,height);
-	noStroke();
-	ellipse(0,height/2,3,3);
-	ellipse(4930,height/2,50,50);
-	ellipse(9216,height/2,50,50);
-	ellipse(12781,height/2,10,10);
-	ellipse(19327,height/2,50,50);
-	ellipse(66320,height/2,50,50);
-	ellipse(122061,height/2,50,50);
-	ellipse(245059,height/2,50,50);
-	ellipse(383560,height/2,50,50);
-	ellipse(500000,height/2,50,50);
-
-
+	//console.log(ship.dist);
+	console.log(windowWidth);
+	console.log(ship.dist);
+	if(ship.dist >= 2 && ship.dist <= windowWidth){
+		console.log("hello");
+		drawPlanet(2);
+	}else if (ship.x > 2000) {
+		//ship.dist += ship.updateDist();
+		console.log(ship.dist);
+		ship.x = 100;
+	}
 
 
 	checkKeyDown();
 }
+
+function drawPlanet(shipX){
+	fill(255);
+	ellipse(shipX+windowWidth,height/2,50,50);
+}
+
 //var count = 0 ;
 function checkKeyDown() {
 	if (keyIsDown(RIGHT_ARROW)) {
-		if(deltaSpeed < 200)
-		deltaSpeed = deltaSpeed +1 *2
-		//console.log(deltaSpeed);
-		count++;
+		if(deltaSpeed < 20)
+		deltaSpeed = deltaSpeed +1 *2;
 		ship.direction(1);
 	} 
 	if (keyIsDown(LEFT_ARROW)) {
-		
-		//while(deltaSpeed > -20 && ship.x > 1)
-		deltaSpeed = deltaSpeed +1 *-2
-		
+		deltaSpeed = deltaSpeed +1 *-2;
 	} 
 }
 
 function drawScale(){
-	for (let i = 0; i < 5000; i++){
+	for (let i = 0; i < 30; i++){
 		fill(0);
 		text(i,100*i,height-60);
 		rect(100*i,height-50,1,50);
