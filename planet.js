f67b58d83cbf2b266828e730d81c55fad420ffba
Keaton Armstrong <keatondirect@icloud.com>
2018-03-28 00:13:47 -0700

    deleted space

--- planet.js
+++ planet.js
@@ -1,7 +1,7 @@
 function Planet(){    
     //draws a planet
     this.showPlanet = function(planet){
-        if(ship.dist >= planet-((windowWidth+100)-ship.x) && ship.dist <= planet +ship.x){
+        if(ship.dist >= planet-((windowWidth+100)-ship.x) && ship.dist <= planet+ship.x){
             fill(255,0,0);
             ellipse(windowWidth,100,100,100);
         }
